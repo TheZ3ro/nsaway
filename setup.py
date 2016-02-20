480fde1fe31efe14e5e2c62a0eb9d8ff30a87bf3
thez3ro <silvethebest@yahoo.it>
2016-02-20 18:27:57 +0100

    working on the setup

--- setup.py
+++ setup.py
@@ -1,42 +1,63 @@
-# TODO
-# Find a better distribution tool. I HATE PYTHON ALREADY.
+#!/usr/bin/env python
 
-'''
-# Based on setup.py from https://github.com/pypa/sampleproject
+# Author: TheZero <io@thezero.org>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-from setuptools import setup, find_packages
+import sys, os
+from nsaway.nsaway import __version__
+from nsaway.utils import SETTINGS_FILE, ICON_PATH
 
-setup(
-    name="nsaway",
-    version="0.1.0",
-    description="NSAway.",
-    long_description="For a detailed description, see https://github.com/TheZ3ro/nsaway.",
-    url="https://github.com/TheZ3ro/nsaway",
-    author="TheZero",
-    author_email="io@thezero.org",
-    license="GPLv3",
-    classifiers=[
-        "Development Status :: 3 - Alpha",
-        "Intended Audience :: End Users/Desktop",
-        "Intended Audience :: System Administrators",
-        "Topic :: Utilities",
-        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
-        "Operating System :: POSIX :: Linux",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.2",
-        "Programming Language :: Python :: 3.3",
-        "Programming Language :: Python :: 3.4",
-        "Programming Language :: Python :: 3.5",
+setup = {
+    "name":"nsaway",
+    "version":__version__,
+    "description":"NSAway.",
+    "long_description":"For a detailed description, see https://github.com/TheZ3ro/nsaway.",
+    "url":"https://github.com/TheZ3ro/nsaway",
+    "author":"TheZero",
+    "author_email":"io@thezero.org",
+    "license":"GPLv3",
+    "dest_dir":"",
+    "data_files":[
+      (SETTINGS_FILE, ['config/nsaway.ini']),
+      (ICON_PATH, ['icons/nsaway_large.png','icons/nsaway_mini.png','icons/nsaway.png'])
     ],
-    keywords="ids intrusion detection system",
-    packages=find_packages(),
-    data_files=[('/etc/nsaway.ini', ['config/nsaway.ini']),
-                ('/usr/share/pixmaps/nsaway/', ['icons/nsaway_large.png','icons/nsaway_mini.png','icons/nsaway.png'])],
-    entry_points={
-        "console_scripts": [
-            "nsaway = nsaway.nsaway:main",
-        ],
-    },
-)
-'''
+    "scripts":[
+      ['nsaway','nsaway.py']
+    ]
+}
+
+help_message = """
+Setup for {0}-{1} by {3}<{4}>
+Released under {5} license
+Project Link {2}
+
+Options:
+  -h --help:         Show this help
+  build:             Build a DEB package
+  install:           Install {0}
+""".format(setup["name"],setup["version"],setup["url"],setup["author"],setup["author_email"],setup["license"])
+
+if __name__=="__main__":
+    args = sys.argv[1:]
+
+    if len(args) == 0:
+      sys.exit(help_message)
+
+    # Check for help
+    if '-h' in args or '--help' in args:
+      sys.exit(help_message)
+
+    print("This setup is custom made. It isn't Python-setuptool compatible.")
+    sys.exit(0)
